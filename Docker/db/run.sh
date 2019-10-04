d6fc476e832542e57779264c656b16681eb5c407
OSCAR MARTIN <elmao79@gmail.com>
2019-10-04 16:54:53 +0200

    MySQL container updated

--- Docker/db/run.sh
+++ Docker/db/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env sh
 
+/entrypoint.sh mysqld &
+
+sleep 5
 echo "GRANT ALL PRIVILEGES ON ${MAGENTO_DATABASE}.* TO ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}'; FLUSH PRIVILEGES" | mysql -u root -p${MYSQL_ROOT_PASSWORD} > /dev/null 2>&1
 
-/entrypoint.sh mysqld
+tail -f /dev/null
